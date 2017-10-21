1105d9dc29589f0723f64b9b394c36c537a0fee8
Javier Calderon <javier.calderon@globant.com>
2017-10-20 21:56:52 -0500

    Mas Literales

--- DataTypeSwift.swift
+++ DataTypeSwift.swift
@@ -2,3 +2,10 @@
 let dedosEnLaMano = 5
 let autosEnLaCochera = 2
 let estrellasEnElCielo = 1_000_000 // Puedes usar _ entre números para mejorar la legibilidad
+
+let decimal = 17
+let binario = 0b10001       // 17 en notación binaria
+let octal = 0o21            // 17 en notación octal
+let hexadecimal = 0x11      // 17 en notación hexadecimal
+
+let suma = decimal + octal  // #=> 34
