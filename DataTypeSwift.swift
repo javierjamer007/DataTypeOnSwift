1475aa34d965eab3fb7a7c1b69ff61f32ed82059
Javier Calderon <javier.calderon@globant.com>
2017-10-20 22:50:52 -0500

    Ajustes

--- DataTypeSwift.swift
+++ DataTypeSwift.swift
@@ -3,12 +3,12 @@ let dedosEnLaMano = 5
 let autosEnLaCochera = 2
 let estrellasEnElCielo = 1_000_000 // Puedes usar _ entre números para mejorar la legibilidad
 
-let decimal = 17
+let decimalInt = 17
 let binario = 0b10001       // 17 en notación binaria
 let octal = 0o21            // 17 en notación octal
-let hexadecimal = 0x11      // 17 en notación hexadecimal
+let hexadecimalInt = 0x11      // 17 en notación hexadecimal
 
-let suma = decimal + octal  // #=> 34
+let suma = decimalInt + octal  // #=> 34
 
 // Double
 
