80186dabb7d0e483f1653d5ac2c46ba3662d9296
Javier Calderon <javier.calderon@globant.com>
2017-10-20 22:45:21 -0500

    File with error

--- DataTypeSwift.swift
+++ DataTypeSwift.swift
@@ -3,12 +3,12 @@ let dedosEnLaMano = 5
 let autosEnLaCochera = 2
 let estrellasEnElCielo = 1_000_000 // Puedes usar _ entre números para mejorar la legibilidad
 
-let decimal = 17
+let decimalInt = 17
 let binario = 0b10001       // 17 en notación binaria
 let octal = 0o21            // 17 en notación octal
 let hexadecimal = 0x11      // 17 en notación hexadecimal
 
-let suma = decimal + octal  // #=> 34
+let suma = decimalInt + octal  // #=> 34
 
 // Float
 
@@ -19,4 +19,4 @@ let gramos: Float = 85.32
 
 let decimal = 12.1875
 let exponente = 1.21875e1
-let hexadecimal = 0xC.3p0
+let hexadecimalInt = 0xC.3p0
