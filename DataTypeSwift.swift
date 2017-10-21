facb6bdadac405fd3ff497f314256db987d814fc
Javier Calderon <javier.calderon@globant.com>
2017-10-20 22:17:14 -0500

    Float Type

--- DataTypeSwift.swift
+++ DataTypeSwift.swift
@@ -9,3 +9,7 @@ let octal = 0o21            // 17 en notación octal
 let hexadecimal = 0x11      // 17 en notación hexadecimal
 
 let suma = decimal + octal  // #=> 34
+
+// Float
+
+let gramos: Float = 85.32
